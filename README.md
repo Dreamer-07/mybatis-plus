13138ed40de4c87000bd36d397522f76025255f0
EMTKnight <2391105059@qq.com>
2021-03-15 11:14:47 +0800

    代码自动生成器(2)

--- README.md
+++ README.md
@@ -268,7 +268,7 @@ public enum IdType {
 
 > 方式一：数据库级别(不推荐，开发中一般不允许修改数据库)
 
-1. 修改数据库表结构：添加 `create_time` `update_time` 字段
+1. 修改数据库表结构：添加 `create_time` `update_time` 字段，默认值设置为 **CURRENT_TIMESTAMP**
 
    ![image-20210311190420353](README.assets/image-20210311190420353.png)
 
@@ -831,5 +831,73 @@ public void testDelete(){
   </dependency>
   ```
 
-- 
+- 参考 [MP 官方文档](https://mp.baomidou.com/config/generator-config.html#%E5%9F%BA%E6%9C%AC%E9%85%8D%E7%BD%AE) 配置需要的选项后执行
+
+  ```java
+  // 1 创建代码自动生成器对象
+  AutoGenerator ag = new AutoGenerator();
+  
+  // 2 创建全局配置对象
+  GlobalConfig gc = new GlobalConfig();
+  // 2.1 设置代码生成位置
+  String projectPath = System.getProperty("user.dir"); // 获取当前项目的路径位置
+  gc.setOutputDir(projectPath + "/src/main/java");
+  gc.setAuthor("Dreamer07"); // 设置作者
+  gc.setOpen(false); // 不打开 Win 资源管理器
+  gc.setFileOverride(false); // 设置不覆盖已有文件
+  gc.setServiceName("%sService"); // 去除 Service 接口开头的 I
+  gc.setIdType(IdType.AUTO); // 设置主键策略
+  ag.setGlobalConfig(gc);
+  
+  // 3 设置数据库配置
+  DataSourceConfig dsc = new DataSourceConfig();
+  dsc.setDbType(DbType.MYSQL);
+  dsc.setUsername("root");
+  dsc.setPassword("Dreamer07");
+  dsc.setUrl("jdbc:p6spy:mysql://localhost:3306/mybatis_plus?useSSL=false&useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC");
+  dsc.setDriverName("com.p6spy.engine.spy.P6SpyDriver");
+  ag.setDataSource(dsc);
+  
+  
+  // 4. 策略配置
+  StrategyConfig sc = new StrategyConfig();
+  // 下划线转驼峰命名
+  sc.setNaming(NamingStrategy.underline_to_camel);
+  sc.setColumnNaming(NamingStrategy.underline_to_camel);
+  // setInclude() 设置要为哪些数据表生成实体
+  sc.setInclude("user");
+  // setEntityLombokModel() 设置是否使用 Lombok 注解
+  sc.setEntityLombokModel(true);
+  // setControllerMappingHyphenStyle() 开启 Controller 请求映射的下划线命名
+  sc.setControllerMappingHyphenStyle(true);
+  // setEntityTableFieldAnnotationEnable() 生成字段注解
+  sc.setEntityTableFieldAnnotationEnable(true);
+  // setVersionFieldName() 设置乐观锁字段名
+  sc.setVersionFieldName("version");
+  // setLogicDeleteFieldName() 设置逻辑删除字段名
+  sc.setLogicDeleteFieldName("deleted");
+  // setTableFillList() 设置填充字段集合
+  sc.setTableFillList(Arrays.asList(
+      new TableFill("create_time", FieldFill.INSERT),
+      new TableFill("update_time", FieldFill.INSERT_UPDATE)
+  ));
+  ag.setStrategy(sc);
+  
+  // 5. 配置包信息
+  PackageConfig pc = new PackageConfig();
+  // 设置父包
+  pc.setParent("pers.dreamer07");
+  // 设置模块名
+  pc.setModuleName("cg");
+  pc.setEntity("pojo");
+  ag.setPackageInfo(pc);
+  
+  
+  // 5. 执行代码自动生成器
+  ag.execute();
+  ```
+
+- 测试结果
+
+  ![image-20210315111014067](README.assets/image-20210315111014067.png)
 
--- src/test/java/pers/dreamer07/CodeGenerator.java
+++ src/test/java/pers/dreamer07/CodeGenerator.java
@@ -1,12 +1,27 @@
 package pers.dreamer07;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.po.TableFill;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.mysql.cj.jdbc.Driver;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
 
 /**
  * @program: mybatis-plus
@@ -16,9 +31,6 @@ import javax.sql.DataSource;
  **/
 @SpringBootTest
 public class CodeGenerator {
-
-    @Autowired
-    private DataSource dataSource;
 
     public static void main(String[] args) {
         // 1 创建代码自动生成器对象
@@ -31,11 +43,58 @@ public class CodeGenerator {
         gc.setOutputDir(projectPath + "/src/main/java");
         gc.setAuthor("Dreamer07"); // 设置作者
         gc.setOpen(false); // 不打开 Win 资源管理器
+        gc.setFileOverride(false); // 设置不覆盖已有文件
+        gc.setServiceName("%sService"); // 去除 Service 接口开头的 I
+        gc.setIdType(IdType.AUTO); // 设置主键策略
         ag.setGlobalConfig(gc);
 
         // 3 设置数据库配置
         DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setDbType(DbType.MYSQL);
+        dsc.setUsername("root");
+        dsc.setPassword("Dreamer07");
+        dsc.setUrl("jdbc:p6spy:mysql://localhost:3306/mybatis_plus?useSSL=false&useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC");
+        dsc.setDriverName("com.p6spy.engine.spy.P6SpyDriver");
         ag.setDataSource(dsc);
+
+
+        // 4. 策略配置
+        StrategyConfig sc = new StrategyConfig();
+        // 下划线转驼峰命名
+        sc.setNaming(NamingStrategy.underline_to_camel);
+        sc.setColumnNaming(NamingStrategy.underline_to_camel);
+        // setInclude() 设置要为哪些数据表生成实体
+        sc.setInclude("user");
+        // setEntityLombokModel() 设置是否使用 Lombok 注解
+        sc.setEntityLombokModel(true);
+        // setControllerMappingHyphenStyle() 开启 Controller 请求映射的下划线命名
+        sc.setControllerMappingHyphenStyle(true);
+        // setEntityTableFieldAnnotationEnable() 生成字段注解
+        sc.setEntityTableFieldAnnotationEnable(true);
+        // setVersionFieldName() 设置乐观锁字段名
+        sc.setVersionFieldName("version");
+        // setLogicDeleteFieldName() 设置逻辑删除字段名
+        sc.setLogicDeleteFieldName("deleted");
+        // setTableFillList() 设置填充字段集合
+        sc.setTableFillList(Arrays.asList(
+                new TableFill("create_time", FieldFill.INSERT),
+                new TableFill("update_time", FieldFill.INSERT_UPDATE)
+        ));
+        ag.setStrategy(sc);
+
+        // 5. 配置包信息
+        PackageConfig pc = new PackageConfig();
+        // 设置父包
+        pc.setParent("pers.dreamer07");
+        // 设置模块名
+        pc.setModuleName("cg");
+        pc.setEntity("pojo");
+        ag.setPackageInfo(pc);
+
+
+        // 5. 执行代码自动生成器
+        ag.execute();
     }
+
 
 }
